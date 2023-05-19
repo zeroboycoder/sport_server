0716cfd3c07d37de887af2de1410ba7d4d711957
zeroboycoderan <pyaesonekhant.anbuchi@gmail.com>
2023-05-19 14:02:22 +0630

    Finished to configure CRUD with database and Swagger Integration

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 const route = require("express").Router();
+const userController = require("../controllers/user");
 
 route.get("/", (req, res) => res.send("Hello World"));
+
+route.post("/register", userController.registerUser);
+
+route.post("/signin", userController.signinUser);
+
+route.get("/users", userController.fetchUsers);
+
+route.put("/update-user/:userId", userController.updateUser);
+
+route.delete("/delete-user/:userId", userController.deleteUser);
 
 module.exports = route;
