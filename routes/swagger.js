b9344b348164de74a16589882b2f3f375437bb7a
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2023-06-07 03:34:38 +0630

    Finished to write api docs for auth and user

--- routes/swagger.js
+++ routes/swagger.js
@@ -7,26 +7,58 @@ const route = require("express").Router();
  *      User:
  *          type: object
  *          required:
- *              - name
- *              - email
- *              - password
+ *              - phone
+ *              - token
+ *              - user_code
+ *              - status
+ *              - ban_end_date
+ *              - agent
+ *              - role
+ *              - wallet
  *          properties:
  *              id:
  *                  type: int
  *                  description: User ID
- *              name:
+ *              token:
  *                  type: string
- *                  description: User Name
- *              email:
+ *                  description: JWT Token
+ *              user_code:
  *                  type: string
- *                  description: User Email
- *              password:
- *                  type: string
- *                  description: User Password
+ *                  description: User Code
+ *              status:
+ *                  type: boolean
+ *                  description: User Status
+ *              ban_end_date:
+ *                  type: DateTime
+ *                  description: Ban End Date
+ *              agent:
+ *                 type: object
+ *                 description: Agent Data
+ *              role:
+ *                  type: object
+ *                  description: Role Data
+ *              wallet:
+ *                  type: object
+ *                  description: Wallet Data
  *          example:
- *              name: Mg Mg
- *              email: mgmg@email.com
- *              password : Passw0rd
+ *              token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjYsImlhdCI6MTY4NjA4MTM3NX0.gw4-Uup-9Ru7sQ0ztYS4ov9jJIggku2QK8oBOzr759Y
+ *              user_code: mgmg56
+ *              status: true
+ *              ban_end_date: null
+ *              agent:
+ *                id : 1
+ *                name : Mg Mg
+ *                address : yangon
+ *                secret_code : 6ki7lbg1
+ *                userId: 1
+ *              role:
+ *                id : 1
+ *                name : agent
+ *              wallet:
+ *                id : 1
+ *                type : main
+ *                amount : 120000
+ *                userId : 1
  */
 
 /**
@@ -38,19 +70,38 @@ const route = require("express").Router();
 
 /**
  * @swagger
- * /register:
+ * tags:
+ *  name: User
+ *  description: User Information
+ */
+
+// =====================
+// Authentication
+// =====================
+/**
+ * @swagger
+ * /create-agent:
  *  post:
- *      summary: Register the user.
+ *      summary: Create an agent.
  *      tags: [Auth]
  *      requestBody:
  *          required: true
  *          content:
  *              application/json:
  *                  schema:
- *                      $ref: "#/components/schemas/User"
+ *                      type: object
+ *                      properties:
+ *                        phone:
+ *                          type: string
+ *                        password:
+ *                          type: string
+ *                        name:
+ *                          type: string
+ *                        address:
+ *                          type: string
  *      responses:
  *          200:
- *              description: User Registration.
+ *              description: Agent created successfully.
  *              content:
  *                  application/json:
  *                      schema:
@@ -59,19 +110,130 @@ const route = require("express").Router();
 
 /**
  * @swagger
- * /signin:
+ * /signin-agent:
  *  post:
- *      summary: User Login.
+ *      summary: Sign in agent.
  *      tags: [Auth]
  *      requestBody:
  *          required: true
  *          content:
  *              application/json:
  *                  schema:
- *                      $ref: "#/components/schemas/User"
+ *                      type: object
+ *                      properties:
+ *                        phone:
+ *                          type: string
+ *                        password:
+ *                          type: string
  *      responses:
  *          200:
- *              description: user Login.
+ *              description: Agent created successfully.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#/components/schemas/User"
+ */
+
+/**
+ * @swagger
+ * /update-agent/{userId}:
+ *  put:
+ *      summary: Update agent data
+ *      tags: [Auth]
+ *      parameters:
+ *          -   in: path
+ *              name : agentId
+ *              schema:
+ *                  type: int
+ *              required: true
+ *              description: The agent user id
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                        name:
+ *                          type: string
+ *                        phone:
+ *                          type: string
+ *                        address:
+ *                          type: string
+ *      responses:
+ *          200:
+ *              description: Update succesfully
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#/components/schemas/User"
+ *          404:
+ *              description:  Book not found
+ *          400:
+ *              description: Something wrong in server
+ */
+
+/**
+ * @swagger
+ * /delete-agent/{userId}:
+ *  delete:
+ *      summary: Delete the agent
+ *      tags: [Auth]
+ *      parameters:
+ *          -   in: path
+ *              name : agentId
+ *              schema:
+ *                  type: int
+ *              required: true
+ *              description: The agent user id
+ *      responses:
+ *          200:
+ *              description: Delete succesfully
+ *          404:
+ *              description: Book not found
+ */
+
+// =====================
+// User Information
+// =====================
+/**
+ * @swagger
+ * /init:
+ *  post:
+ *      summary: Get member when init app.
+ *      tags: [User]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                        player_id:
+ *                          type: string
+ *                        player_name:
+ *                          type: string
+ *                        unit_amount:
+ *                          type: string
+ *                        agent_code:
+ *                          type: string
+ *      responses:
+ *          200:
+ *              description: .
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#/components/schemas/User"
+ */
+
+/** @swagger
+ * /agent-profile/{userId}:
+ *  get:
+ *      summary: Get the one agent data.
+ *      tags: [User]
+ *      responses:
+ *          200:
+ *              description: Successful.
  *              content:
  *                  application/json:
  *                      schema:
